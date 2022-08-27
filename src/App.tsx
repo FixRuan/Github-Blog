1d2d12a91e4c2a2d20eff27918c0784d67fce2ed
Ruan Pablo <ruan_goio10@hotmail.com>
2022-08-27 16:21:39 -0300

    Global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Home } from "./pages/Home";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 
 function App() {
 	return (
-		<Home />
+		<BrowserRouter>
+			<Router />
+		</BrowserRouter>
 	);
 }
 
