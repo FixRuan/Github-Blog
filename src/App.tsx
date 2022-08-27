88ae121689e8e0d00a00b21a34435525e1601d5a
Ruan Pablo <ruan_goio10@hotmail.com>
2022-08-27 15:54:11 -0300

    Eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+import { Home } from "./pages/Home";
 
 
 function App() {
-  return (
-    <div className="text-2xl font-bold">Hello World</div>
-  )
+	return (
+		<Home />
+	);
 }
 
-export default App
+export default App;
